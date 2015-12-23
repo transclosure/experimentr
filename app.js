2ee676cdcd8b4b22e7649ba5c7b4f88dd5dc609d
ryandanas <ryandanas@wpi.edu>
2015-12-23 16:48:01 -0500

    updating ports

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var express     = require('express')
   , http        = require('http')
   , redis       = require('redis')
   , redisClient
-  , port        = process.argv[2] || 8000
-  , rport       = process.argv[3] || 6379
+  , port        = process.argv[2] || 9001
+  , rport       = process.argv[3] || 9000
   , debug       = process.argv[4] || null
 
 // Database setup
