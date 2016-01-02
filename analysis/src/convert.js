62be9e4891d4d7421cc983a026b5e16cbc32f3fc
ryandanas <ryandanas@wpi.edu>
2016-01-02 20:27:49 +0000

    backing up data and updating convert to include aux data

--- analysis/src/convert.js
+++ analysis/src/convert.js
@@ -31,12 +31,20 @@ function getFields(spec, phrase, scenario) {
 		'scenario',
 		'_clicks_f??'+phrase+scenario,
 		'_time_f??'+phrase+scenario,
+		'_clicktime_f??'+phrase+scenario,
+		'_scenariotime_f??'+phrase+scenario,
 		'_clicks_t??'+phrase+scenario,
 		'_time_t??'+phrase+scenario,
+		'_clicktime_t??'+phrase+scenario, 
+                '_scenariotime_t??'+phrase+scenario,
 		'_clicks_'+spec+p1+s1+phrase+scenario,
 		'_time_'+spec+p1+s1+phrase+scenario,
+		'_clicktime_'+spec+p1+s1+phrase+scenario, 
+                '_scenariotime_'+spec+p1+s1+phrase+scenario,
 		'_clicks_'+spec+p2+s2+phrase+scenario,
-		'_time_'+spec+p2+s2+phrase+scenario ];
+		'_time_'+spec+p2+s2+phrase+scenario ]
+		'_clicktime_'+spec+p2+s2+phrase+scenario,
+                '_scenariotime_'+spec+p2+s2+phrase+scenario;
 
 }
 
