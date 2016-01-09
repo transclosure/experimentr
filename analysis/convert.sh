df25d7e4bf9a01e9f00cd70b96dc2b1fad85cd05
ryandanas <ryandanas@wpi.edu>
2016-01-09 15:05:33 -0500

    the other half of the permutations of our first experiment complete. running trial 3 of 4 now.

--- analysis/convert.sh
+++ analysis/convert.sh
@@ -1,13 +1,17 @@
 echo "workerId,postId,experimentID,formerTask,latterTask,formerClicks,formerTime,formerClickorder,formerClicktime,formerScenarioTime,latterClicks,latterTime,latterClickorder,latterClicktime,latterScenarioTime" > results/data.csv
 
-node src/convert.js results/data.json 	GOM,AOM,GRC,ARC		GOM		GRC 	>> results/data.csv
-node src/convert.js results/data.json 	GOM,AOM,GRC,ARC		AOM		ARC 	>> results/data.csv
+node src/convert.js results/data.json 	GOM,AOM,GRC,ARC		teach	walkthrough 	>> results/data.csv
+node src/convert.js results/data.json 	GOM,AOM,GRC,ARC		GOM		GRC 			>> results/data.csv
+node src/convert.js results/data.json 	GOM,AOM,GRC,ARC		AOM		ARC 			>> results/data.csv
 
-node src/convert.js results/data.json 	GRC,ARC,GOM,AOM 	GOM		GRC 	>> results/data.csv
-node src/convert.js results/data.json 	GRC,ARC,GOM,AOM 	AOM		ARC 	>> results/data.csv
+node src/convert.js results/data.json 	GRC,ARC,GOM,AOM		teach	walkthrough 	>> results/data.csv
+node src/convert.js results/data.json 	GRC,ARC,GOM,AOM 	GOM		GRC 			>> results/data.csv
+node src/convert.js results/data.json 	GRC,ARC,GOM,AOM 	AOM		ARC 			>> results/data.csv
 
-node src/convert.js results/data.json 	GOC,AOC,GRM,ARM 	GRM		GOC 	>> results/data.csv
-node src/convert.js results/data.json 	GOC,AOC,GRM,ARM 	ARM		AOC 	>> results/data.csv
+node src/convert.js results/data.json 	GOC,AOC,GRM,ARM		teach	walkthrough 	>> results/data.csv
+node src/convert.js results/data.json 	GOC,AOC,GRM,ARM 	GRM		GOC 			>> results/data.csv
+node src/convert.js results/data.json 	GOC,AOC,GRM,ARM 	ARM		AOC 			>> results/data.csv
 
-node src/convert.js results/data.json 	GRM,ARM,GOC,AOC 	GRM		GOC 	>> results/data.csv
-node src/convert.js results/data.json 	GRM,ARM,GOC,AOC 	ARM		AOC 	>> results/data.csv
+node src/convert.js results/data.json 	GRM,ARM,GOC,AOC		teach	walkthrough 	>> results/data.csv
+node src/convert.js results/data.json 	GRM,ARM,GOC,AOC 	GRM		GOC 			>> results/data.csv
+node src/convert.js results/data.json 	GRM,ARM,GOC,AOC 	ARM		AOC 			>> results/data.csv
