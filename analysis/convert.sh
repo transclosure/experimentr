32dd4ac0dd11497a84466fa4ab01a927f73a59ee
ryandanas <ryandanas@gmail.com>
2016-09-19 12:32:10 -0400

    misconversion of pilot data. fixed.

--- analysis/convert.sh
+++ analysis/convert.sh
@@ -1,2 +1,2 @@
 node src/convert.js results/data.json modules/AOW.html  >> results/data.csv
-node src/convert.js results/data.json modules/GOM.html  >> results/data.csv
+node src/convert.js results/data.json modules/HOM.html  >> results/data.csv
