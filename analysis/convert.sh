5675fcbd8e92a4c8e96066380658b904a19df476
ryandanas <ryandanas@gmail.com>
2016-11-21 13:21:46 -0500

    correlation pilot redux in

--- analysis/convert.sh
+++ analysis/convert.sh
@@ -5,4 +5,6 @@ node src/convert.js results/data.json modules/t4.html  >> results/data.csv
 node src/convert.js results/data.json modules/t5.html  >> results/data.csv
 node src/convert.js results/data.json modules/t6.html  >> results/data.csv
 node src/convert.js results/data.json modules/t7.html  >> results/data.csv
-node src/convert2.js results/data.json modules/t8.html >> results/data.csv
+node src/convert.js results/data.json modules/t8.html  >> results/data.csv
+node src/convert.js results/data.json modules/t9.html  >> results/data.csv
+node src/convert2.js results/data.json modules/t10.html >> results/data.csv
