5cb46b8fa866e10ca5dcf2297452cc6b79c608b6
transclosure <natasha_danas@brown.edu>
2017-03-26 21:12:58 -0400

    minor fix

--- analysis/convert.sh
+++ analysis/convert.sh
@@ -10,3 +10,10 @@ node src/convert.js results/data.json modules/g09.html  >> results/data.csv
 node src/convert2.js results/data.json modules/gprov.html >> results/data.csv
 node src/convert2.js results/data.json modules/gcore.html >> results/data.csv
 
+node src/convert.js results/data.json modules/a01.html  >> results/data.csv
+node src/convert.js results/data.json modules/a02.html  >> results/data.csv
+node src/convert.js results/data.json modules/a03.html  >> results/data.csv
+node src/convert.js results/data.json modules/a04.html  >> results/data.csv
+node src/convert2.js results/data.json modules/aprov.html >> results/data.csv
+node src/convert2.js results/data.json modules/acore.html >> results/data.csv
+
