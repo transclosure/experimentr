313c1456385ff9c1833aaacaf6eeac3cb116754a
ryandanas <ryandanas@gmail.com>
2017-11-08 23:44:06 -0500

    ready to go!

--- analysis/convert.sh
+++ analysis/convert.sh
@@ -1,1 +1,6 @@
 node src/convert.js results/data.json tasks/gcd.html  >> results/data.csv
+node src/convert.js results/data.json tasks/hypotenuse.html  >> results/data.csv
+node src/convert.js results/data.json tasks/adding-machine.html  >> results/data.csv
+node src/convert.js results/data.json tasks/title.html  >> results/data.csv
+node src/convert.js results/data.json tasks/first-half.html  >> results/data.csv
+node src/convert.js results/data.json tasks/cut-at-xx.html  >> results/data.csv
