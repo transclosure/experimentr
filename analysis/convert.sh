8e20566754765771c59b621678748aeb555691f6
ryandanas <ryandanas@gmail.com>
2017-11-16 19:49:58 -0500

    priming+incentive study 1 debugged and ready to go.

--- analysis/convert.sh
+++ analysis/convert.sh
@@ -1,5 +1,5 @@
-node src/convert.js results/data.json tasks/double-INT.html  >> results/data.csv
-node src/convert.js results/data.json tasks/double-FLOAT.html  >> results/data.csv
-node src/convert.js results/data.json tasks/adding-machine-POS.html >> results/data.csv
-node src/convert.js results/data.json tasks/adding-machine-NEG.html >> results/data.csv
+node src/convert.js results/data.json tasks/adding-machine-POS-INT.html  >> results/data.csv
+node src/convert.js results/data.json tasks/adding-machine-NEG-INT.html  >> results/data.csv
+node src/convert.js results/data.json tasks/adding-machine-POS-FLOAT.html >> results/data.csv
+node src/convert.js results/data.json tasks/adding-machine-NEG-FLOAT.html >> results/data.csv
 
