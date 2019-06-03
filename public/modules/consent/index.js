03b4570de20f806d86b8cb2c0849a25e0015ca15
transclosure <natasha_danas@brown.edu>
2019-06-03 14:22:26 -0400

    first discriminative NL-LTL-trace study ready to go out. testing analysis scripts now.

--- public/modules/consent/index.js
+++ public/modules/consent/index.js
@@ -1,38 +1,35 @@
-(function() {
-  var data    = {}
-    , workers = null;
+var id    = null;
+var workers = null;
 
-  init();
+init();
 
-  function init() {
-    experimentr.hideNext();
+function init() {
+  experimentr.hideNext();
 
-    // load previous workers file
-    d3.json('modules/consent/blocked-workers.json', function(err, d) {
-      workers = d;
-      d3.select('#workerId').attr('disabled', null);
-    });
+  // load previous workers file
+  d3.json('modules/consent/blocked-workers.json', function(err, d) {
+    workers = d;
+    d3.select('#workerId').attr('disabled', null);
+  });
 
-    d3.selectAll('#workerId')
-      .on('keypress', function() { data.workerId = this.value.trim().toUpperCase(); })
-      .on('blur', function() { data.workerId = this.value.trim().toUpperCase(); });
+  d3.selectAll('#workerId')
+    .on('keypress', function() { id = this.value.trim().toUpperCase(); })
+    .on('blur', function() { id = this.value.trim().toUpperCase(); });
 
-    d3.select('#consentYes').on('click', experimentr.next);
+  d3.select('#consentYes').on('click', experimentr.next);
 
-    d3.select('#checkId').on('click', validate);
-  }
+  d3.select('#checkId').on('click', validate);
+}
 
-  function validate() {
-    if( data.workerId ) {
-      experimentr.addData(data);
+function validate() {
+  if( id ) {
+    experimentr.addData(data);
 
-      if( workers.indexOf(data.workerId) == -1 ) {
-        d3.select('#consentYes').attr('disabled', null);
-      } else {
-        d3.select('#invalidMessage').style('display', 'inline');
-      }
+    if( workers.indexOf(id) == -1 ) {
+      d3.select('#consentYes').attr('disabled', null);
+      data["id"] = id;
+    } else {
+      d3.select('#invalidMessage').style('display', 'inline');
     }
   }
-
-}());
-
+}
